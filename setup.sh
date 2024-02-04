f5e8f5fb121fec351b4e6158bdd7b98f5b74ef20
wibus-wee <1596355173@qq.com>
2024-02-04 17:04:17 +0800

    first commit

---
+++ setup.sh
@@ -0,0 +1,110 @@
+sudo -v
+
+# System setting
+
+## Disable the warning when changing a file extension
+echo "Disabling the warning when changing a file extension..."
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+
+## Show path bar, and layout as multi-column
+echo "Show path bar, and layout as multi-column..."
+defaults write com.apple.finder ShowPathbar -bool true
+defaults write com.apple.finder FXPreferredViewStyle -string clmv
+
+## Show the status bar
+echo "Show the status bar..."
+defaults write com.apple.finder ShowStatusBar -bool true
+
+## Allow text selection in Quick Look
+echo "Allow text selection in Quick Look..."
+defaults write com.apple.finder QLEnableTextSelection -bool true
+
+## Search in current folder by default
+echo "Search in current folder by default..."
+defaults write com.apple.finder FXDefaultSearchScope -string SCcf
+
+## Enable Develop Menu, Web Inspector
+echo "Enabling Develop Menu, Web Inspector..."
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtras -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
+
+## Avoid creating .DS_Store files on USB or network volumes
+echo "Avoid creating .DS_Store files on USB or network volumes..."
+defaults write com.apple.desktopservices dsdontwriteusbstores -bool true
+defaults write com.apple.desktopservices dsdontwritenetworkstores -bool true
+
+## Disable disk image verification
+echo "Disabling disk image verification..."
+defaults write com.apple.frameworks.diskimages skip-verify -bool true
+defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
+defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
+
+## Disable personalized advertising
+# echo "Disabling personalized advertising..."
+# defaults com.apple.AdLib forceLimitAdTracking -bool true
+# defaults com.apple.AdLib allowApplePersonalizedAdvertising -bool false
+# defaults com.apple.AdLib allowIdentifierForAdvertising -bool false
+
+## Expand save panel by default
+echo "Expanding save panel by default..."
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+
+## Disable Gatekeeper
+sudo spctl --master-disable
+
+# Install Xcode CLI
+install_xcode_cli() {
+  if test ! $(which xcode-select); then
+    echo "Installing Xcode CLI..."
+    xcode-select --install
+  fi
+}
+install_xcode_cli
+
+install_homebrew() {
+  if test ! $(which brew); then
+    echo "Installing Homebrew..."
+    /bin/zsh -c "$(curl -fsSL https://gitee.com/cunkai/HomebrewCN/raw/master/Homebrew.sh)"
+  fi
+}
+install_homebrew
+
+# Install packages
+install_rcm() {
+  if test ! $(which rcup); then
+    echo "Installing rcm..."
+    brew install rcm
+  fi
+}
+install_rcm
+
+cd
+git clone git@github.com:wibus-wee/dotfiles.git .dotfiles
+rcup -t base
+mkdir -p .config
+mv .Brewfile Brewfile
+brew bundle install
+
+# Install Operator Mono Nerd Font
+install_operator_mono_nerd_font() {
+  if test ! -f ~/Library/Fonts/OperatorMonoLigNerdFont-Italic.otf; then
+    echo "Installing Operator Mono Nerd Font..."
+    fonts=https://github.com/wibus-wee-ac/patch-operator-mono-nerd-cron/releases/download/nightly/with-nerd-fonts.zip
+    wget -O /tmp/OperatorMono.zip $fonts
+    unzip /tmp/OperatorMono.zip -d /tmp
+    mv /tmp/out/*.otf ~/Library/Fonts
+  fi
+}
+install_operator_mono_nerd_font
+
+source ~/.zshrc
+
+# Countdown
+echo "Rebooting in 5 seconds..."
+sleep 5
+sudo reboot
---
+++ sign.sh
@@ -0,0 +1,67 @@
+ssh_gen() {
+  if [ -f ~/.ssh/id_ed25519 ]; then
+    echo "The SSH key already exists. Do you want to overwrite it?"
+    select yn in "Yes" "No"; do
+      case $yn in
+        Yes ) break;;
+        No ) return;;
+      esac
+    done
+  fi
+  echo "Generating a new SSH key for GitHub..."
+  ssh-keygen -t ed25519 -C "1596355173@qq.com"
+
+  echo "Starting the ssh-agent in the background..."
+  eval "$(ssh-agent -s)"
+
+  touch ~/.ssh/config
+  echo "Host *
+    AddKeysToAgent yes
+    UseKeychain yes
+    IdentityFile ~/.ssh/id_ed25519" > ~/.ssh/config
+  ssh-add --apple-use-keychain ~/.ssh/id_ed25519
+
+  echo "Copying the SSH key to the clipboard..."
+  pbcopy < ~/.ssh/id_ed25519.pub
+
+  echo "The SSH key has been copied to the clipboard. Please add it to your GitHub account."
+  echo "Opening the GitHub website..."
+  open "https://github.com/settings/keys"
+  echo "Please press any key to test the SSH connection..."
+  read -n 1 -s -r
+  ssh -T git@github.com
+}
+
+gpg_gen() {
+  # 检查 gpg
+  if test ! $(which gpg); then
+    echo "Please install GPG first."
+    open "https://sourceforge.net/p/gpgosx/docu/Download"
+    return
+  fi
+  if [ -f ~/.gnupg/gpg.conf ]; then
+    echo "The GPG key already exists. Do you want to continue?"
+    select yn in "Yes" "No"; do
+      case $yn in
+        Yes ) break;;
+        No ) return;;
+      esac
+    done
+  fi
+  echo "Generating a new GPG key..."
+  gpg --full-generate-key
+
+  echo "Listing the GPG keys..."
+  gpg --list-secret-keys --keyid-format LONG
+
+  echo "Copying the GPG key..."
+  echo "Please enter the GPG key ID:"
+  read gpg_key_id
+  gpg --armor --export $gpg_key_id | pbcopy
+
+  echo "The GPG key has been copied to the clipboard. Please add it to your GitHub account."
+  echo "Opening the GitHub website..."
+  open "https://github.com/settings/keys"
+}
+
+ssh_gen
