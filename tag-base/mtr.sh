314ec83deb74d91c5f1e8eed37ce34cfba5fed39
wibus-wee <1596355173@qq.com>
2024-03-16 21:11:32 +0800

    Update mtr.sh script with brew update and cleanup

--- tag-base/mtr.sh
+++ tag-base/mtr.sh
@@ -1,4 +1,12 @@
 # mtr.sh - MainTaineR script
-echo "MainTaineR script"
+echo "MainTaineR script - @wibus-wee"
+
+## Update & Upgrade brew
+echo "Update & Upgrade brew"
+brew update
+brew upgrade
+
 ## Clean up brew
-brew cleanup --prune=all; brew autoremove
+echo "Clean up brew"
+brew cleanup --prune=all
+brew autoremove
